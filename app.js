da80e0dc8640159ce735ef56d9e995cf6301460e
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-05-20 23:06:20 +0500

    4 Question Complete

---
+++ app.js
@@ -0,0 +1,39 @@
+//Chap # 31 to 34
+
+//Question # 01
+// var todayDate = new Date()
+// console.log(todayDate)
+
+//Question # 02
+// var todayDate = new Date();
+// var monthInNum = todayDate.getMonth()
+// var totalMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+// var presentMonth = totalMonths[monthInNum]
+// alert(presentMonth)
+
+//Question # 03
+// var date = new Date();
+// var dayInNum = date.getDay();
+// var totalDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+// var todayDay = totalDays[dayInNum]
+// console.log(todayDay)
+
+//Question # 04
+// var date = new Date();
+// var dayInNum = date.getDay();
+// switch(dayInNum){
+//     case 0 :
+//         console.log("It’s Fun day");
+//         break;
+//     case 6 :
+//         console.log("It’s Fun day");
+//         break;
+// }
+
+//Question # 05
+
+
+
+
+
+
